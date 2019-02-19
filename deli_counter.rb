60ec19b7b5a5293d412794523396a2976f6d7bf2
Chase Hilbert <chase@heyspacetime.com>
2019-02-19 17:13:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 # Write your code here.
 #
-#
+
 
 
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position
 end
 
 def line(array)
